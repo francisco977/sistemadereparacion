05371718d13e8b537cf3d567ea09783afb8b6651
Francisco Benjamin Valdez <valdezfranciscobenjamin@gmail.com>
2026-04-20 19:45:57 -0300

    corrijo ruta de public

--- server.js/server.js
+++ server.js/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
